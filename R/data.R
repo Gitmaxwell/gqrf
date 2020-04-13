a6fce5e3eea13d65d34cbf43da16e63897a06324
Kane Maxwell <kane.maxwell@outlook.com>
2020-04-13 07:51:23 +0000

    Added data

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Spatially located coal geochemical analysis
+#'
+#' A dataset containing coal geochemical analysis and associated auxiliary information
+#' coalsp.
+#'
+#' @format A list containing two SpatialPonitsDataFrames
+#' \describe{
+#'   \item{train}{Training Data, locations of coal analysis and auxilary data}
+#'   \item{newdata}{New data locations with auxilary data}
+#'   ...
+#' }
+#' @source Maxwell (2020)
+"coalsp"
--- tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(gqrf)
-
-test_check("gqrf")
